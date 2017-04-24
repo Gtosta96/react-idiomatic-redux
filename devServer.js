0a70dc70f79a6063c5408300195848f943081bec
Gtosta96 <gabrieltosta3@gmail.com>
2017-04-24 20:55:16 -0300

    Simplifying the Arrow Functions - 100%

--- devServer.js
+++ devServer.js
@@ -1,8 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
-import config from './webpack.config.babel';
 import Express from 'express';
+import config from './webpack.config.babel';
 
 const app = new Express();
 const port = 3000;
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, error => {
+app.listen(port, (error) => {
   /* eslint-disable no-console */
   if (error) {
     console.error(error);
@@ -25,7 +25,7 @@ app.listen(port, error => {
     console.info(
       '🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.',
       port,
-      port
+      port,
     );
   }
   /* eslint-enable no-console */
