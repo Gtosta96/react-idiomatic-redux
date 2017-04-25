94b6a5bd9eae2eb9ca45f9c5cb3613e6ba2d07fb
Gtosta96 <gabrieltosta3@gmail.com>
2017-04-24 21:28:05 -0300

    Persisting the State to the Local Storage - 100%

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import App from './components/App';
+import { loadState, saveState } from './localStorage';
 
 // Não é recomendavel definir estado total da
 // aplicação em um unico lugar pois dificulta testes e mudancas
-const persistedState = {
-  todos: [{
-    id: '0',
-    text: 'Welcome back!',
-    completed: false,
-  }],
-};
+const persistedState = loadState();
 
 const store = createStore(todoApp, persistedState);
 console.log(store.getState()); //  eslint-disable-line
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos,
+  });
+}, 1000));
 
 render(
   <Provider store={store}>
